44e7712fd3e0b9e24b132bb695884f263da712c6
Daniel Tweedie <djtwd.ix@icloud.com>
2021-01-22 18:53:33 +0000

    Added condition for 4 of the same direction in the row

--- taxicab-geometry.js
+++ taxicab-geometry.js
@@ -4,7 +4,9 @@ const blocksAway = function(directions) {
   let distanceObject = {};
   for (let i = 0; i < directions.length - 1; i++) {
     // turn right conditions
-    if (directions[i] === "right" && directions[i - 2] === "right" && directions[i - 4] === "right") {
+    if (directions[i] === "right" && directions[i - 2] === "right" && directions[i - 4] === "right" && directions[i - 6] === "right") {
+    	finalPosition[0] += directions[i + 1];
+    } else if (directions[i] === "right" && directions[i - 2] === "right" && directions[i - 4] === "right") {
       finalPosition[1] -= directions[i + 1];
     } else if (directions[i] === "right" && directions[i - 2] === "right") {
       finalPosition[0] -= directions[i + 1];
@@ -13,6 +15,9 @@ const blocksAway = function(directions) {
       finalPosition[1] += directions[i + 1];
     } 
     //turn left conditions
+    	else if (directions[i] === "left" && directions[i - 2] === "left" && directions[i - 4] === "left" && directions[i - 6] === "left") {
+      	finalPosition[1] -= directions[i + 1];
+      }
       else if (directions[i] === "left" && directions[i - 2] === "left" && directions[i - 4] === "left") {
       finalPosition[0] -= directions[i + 1];
     } else if (directions[i] === "left" && directions[i - 2] === "left") {
@@ -24,13 +29,8 @@ const blocksAway = function(directions) {
       finalPosition = finalPosition;
     }
   }
-  //set object keys east and north to finalPosition[1] and finalPosition[0] respectively, then return distanceObject
+  //set object keys east and north to finalPosition[1] and finalPosition[2] respectively, then return distanceObject
   distanceObject.east = finalPosition[1];
   distanceObject.north = finalPosition[0];
   return distanceObject;
 };
-
-//checks
-console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
-console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
-console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
