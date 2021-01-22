cbe2f110229517e62a3a5bb1f50b8d88e79c2d51
Daniel Tweedie <djtwd.ix@icloud.com>
2021-01-22 20:33:14 +0000

    Fixed comments grammar and added Queen values for second check

--- queen-threat-detector.js
+++ queen-threat-detector.js
@@ -1,5 +1,9 @@
-let whiteQueen = [3, 3];
-let blackQueen = [5, 5];
+let whiteQueen = [0, 5];
+let blackQueen = [5, 0];
+/* queen values for second check
+let whiteQueen = [0, 0];
+let blackQueen = [5, 7];
+*/
 
 const generateBoard = function (loc1, loc2) {
   let board = [];
@@ -8,7 +12,7 @@ const generateBoard = function (loc1, loc2) {
   for (let i = 0; i < 8; i++) {
     board.push(new Array(8).fill(0));
   }
-  //iterate over board to place Queen's at given location
+  //iterate over board to place Queens at given location
   for (let r = 0; r < board.length; r++) {
     for (let c = 0; c < board[r].length; c++) {
       if (r === loc1[0] && c === loc1[1]) {
@@ -19,11 +23,11 @@ const generateBoard = function (loc1, loc2) {
       }
     }
   }
-  // if row or column match on Queen's location they are a threat
+  // if row or column match on Queens location they are a threat
   if (loc1[0] === loc2[0] || loc1[1] === loc2[1]) {
     isThreat = true;
   }
-  // if difference between row location of Queen's is same as difference between 
+  // if difference between row location of Queens is same as difference between 
   // column position of Queen's they are a threat
   if (Math.abs(loc1[0] - loc2[0]) === Math.abs(loc1[1] - loc2[1])) {
     isThreat = true;
